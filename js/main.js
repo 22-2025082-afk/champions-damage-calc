084b14fc612b3d36e79808296dddbcc7865e91d6
22-2025082-afk <22-2025082@edu.city.izumi.osaka.jp>
2026-07-06 14:17:29 +0900

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// ==========================================
+// Pokémon Champions Damage Calculator
+// Version 0.1.0
+// main.js
+// ==========================================
+
+import { initializeUI } from "./ui.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Pokémon Champions Damage Calculator v0.1.0");
+
+    initializeUI();
+
+});
